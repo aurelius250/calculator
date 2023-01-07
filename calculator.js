61595aab6050e6017c50bffbcc2a8d6252bd7f57
Becky Nasty <99774559+aurelius250@users.noreply.github.com>
2023-01-06 23:04:11 -0800

    Add delete and clear functionality, cleaned code Broke equals sign, added new functions to slim down big function.

--- calculator.js
+++ calculator.js
@@ -30,86 +30,125 @@ function operate(num1, operator, num2){
     }
 }
 
+function numCheck(element){ 
+    return +element === +element;
+}
+
+
 function calculate(){ 
     let displayArray = []; 
     const screen = document.getElementsByClassName("screenDigits")[0];
- 
+    const allButtons = Array.from(document.querySelectorAll("button")); 
+    console.log(allButtons);
+    console.log(displayArray);
+
+    let buttonPressed; 
+        for(let i = 0; i <= allButtons.length - 1; i++){ 
+        allButtons[i].addEventListener("click", (e) => { 
+            buttonPressed = e.target.textContent; 
+            console.log(buttonPressed);
+            buttonPress();
+        }); 
+    }
+    function buttonPress(){ 
+        addToCalculations(buttonPressed, displayArray)
+    }
+    
     function addToScreen(displayArray){ 
         screen.textContent = "";
-        if(displayArray.length == 0){ 
-            screen.textContent = "";
-            return;
-        }
+       
         for(operand of displayArray){ 
-            screen.textContent += operand;
+            screen.textContent += operand.toString();
         }
         console.log(displayArray);
     } 
 
-    function executeEquation(){ 
-        screen.textContent = "";
-        for(let i = 0; i < displayArray.length - 1; i++){ 
-            screen.textContent = operate(+displayArray[i], displayArray[i+1], +displayArray[i+2]).toString();
+    function clearScreen(){ 
         displayArray.length = 0;
-            return;
-        }
+        screen.textContent = "";
+        return;
     }
-    function addToCalculations(buttonPressed, displayArray){ 
 
+    function isOperator(element){ 
+        return(element == "+" || element == "-" || element == "*" || element == "&#247");
+    }
+
+    calculate.clearScreen = clearScreen; 
+
+    function executeEquation(displayArray){ 
+        clearScreen();
+        let result = 0;
+        //Chain the operations. 
+        for(let i = 0; i < displayArray.length - 1; i++){ 
+            result = operate(+displayArray[i], displayArray[i+1], +displayArray[i+2])
+            displayArray = displayArray.splice(i, 3); 
+            i += 3;
+        }
+        addToScreen(displayArray);
+    }
+
+    //Split function to smaller pieces. Dont repeat yourself. 
+    function addToCalculations(buttonPressed, displayArray){ 
         if(displayArray.length == 0){ 
             displayArray[0] = buttonPressed; 
             addToScreen(displayArray); 
             return; 
         }
-        if(buttonPressed == "="){ 
-            executeEquation();
-            return;
-        }
-        if(buttonPressed == "Delete"){ 
-            let toDelete = displayArray[length - 1].split()[this.length - 1]; 
-
-            displayArray.splice(displayArray.length - 1, 1);
-            addToScreen(displayArray);
-            return;
-        }
+        console.log(allButtons);
 
         if(buttonPressed == "Clear"){ 
-            displayArray.length = 0;
-            addToScreen(displayArray);
+            clearScreen(); 
+            return;
+        }
+        
+        if(buttonPressed == "="){ 
+            executeEquation(displayArray);
             return;
         }
 
-        if(buttonPressed == "+" || buttonPressed == "-" || buttonPressed == "*" || buttonPressed == "&#247"){ 
-            let prevOperand = displayArray[displayArray.length - 1];
-            if(prevOperand == "+" || prevOperand == "-" || prevOperand == "*" || prevOperand == "&#247"){ 
-                displayArray[displayArray.length - 1] = buttonPressed;
+        if(buttonPressed == "Delete"){ 
+            console.log("delete detected");
+            if(displayArray.length == 1){ 
+                clearScreen();
             }
-        }
-
-        if(+buttonPressed === +buttonPressed){ 
             if(+displayArray[displayArray.length - 1] === +displayArray[displayArray.length - 1]){ 
-                displayArray[displayArray.length - 1] += buttonPressed
+                if(displayArray[displayArray.length - 1].split("").length > 2){ 
+                    let toDelete = displayArray[displayArray.length - 1];
+                    let splittedDelete = toDelete.split("");
+                    splittedDelete.splice(splittedDelete.length - 1, 1);
+                    displayArray[displayArray.length - 1] = splittedDelete.join("");
+                } 
+                    else { 
+                        displayArray.splice(displayArray.length - 1, 1); 
+                    }
+                }
+            else { 
+                displayArray.splice(displayArray.length - 1, 1); 
+            }
+            addToScreen(displayArray);
+            return;
+        }
+             
+        if(isOperator(buttonPressed)){ 
+            let prevElement = displayArray[displayArray.length - 1];
+            if(isOperator(prevElement)){ 
+                displayArray[displayArray.length - 1] = buttonPressed;
             } else { 
-            displayArray.push(buttonPressed);  
+                displayArray.push(buttonPressed);
             }
         }
-        else{ 
+        if(numCheck(buttonPressed)){ 
+            let prevElement = displayArray[displayArray.length - 1];
+            if(numCheck(prevElement)){ 
+                displayArray[displayArray.length - 1] += buttonPressed
+            } 
+            else{ 
             displayArray.push(buttonPressed);
+            }
         }
+        
         addToScreen(displayArray);
-
     }
+}
 
-    const allButtons = Array.from(document.querySelectorAll("button")); 
-
-    for(let i = 0; i < allButtons.length - 1; i++){ 
-        allButtons[i].addEventListener("click", (e) => { 
-            const buttonPressed = e.target.textContent; 
-            addToCalculations(buttonPressed, displayArray);
-        }); 
-    }
-
-    }
-const intOne = 5; 
-const intTwo = 2; 
 calculate();
