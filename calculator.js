54ad2d2c715085aec7e3478baa66eb7a705c5ccd
Becky Nasty <99774559+aurelius250@users.noreply.github.com>
2023-01-06 17:02:00 -0800

    Add basic calculator operations

--- calculator.js
+++ calculator.js
@@ -32,21 +32,53 @@ function operate(num1, operator, num2){
 
 function calculate(){ 
     let displayArray = []; 
+    const screen = document.getElementsByClassName("screenDigits")[0];
  
-    function addToScreen(buttonPressed, displayArray){ 
-        const screen = document.getElementsByClassName("screenDigits")[0];
+    function addToScreen(displayArray){ 
         screen.textContent = "";
+        if(displayArray.length == 0){ 
+            screen.textContent = "";
+            return;
+        }
         for(operand of displayArray){ 
             screen.textContent += operand;
         }
         console.log(displayArray);
     } 
-    
-    function numberChecker(elementOne, elementTwo){ 
-        return +elementOne === +elementTwo; 
+
+    function executeEquation(){ 
+        screen.textContent = "";
+        for(let i = 0; i < displayArray.length - 1; i++){ 
+            screen.textContent = operate(+displayArray[i], displayArray[i+1], +displayArray[i+2]).toString();
+        displayArray.length = 0;
+            return;
+        }
     }
     function addToCalculations(buttonPressed, displayArray){ 
-        
+
+        if(displayArray.length == 0){ 
+            displayArray[0] = buttonPressed; 
+            addToScreen(displayArray); 
+            return; 
+        }
+        if(buttonPressed == "="){ 
+            executeEquation();
+            return;
+        }
+        if(buttonPressed == "Delete"){ 
+            let toDelete = displayArray[length - 1].split()[this.length - 1]; 
+
+            displayArray.splice(displayArray.length - 1, 1);
+            addToScreen(displayArray);
+            return;
+        }
+
+        if(buttonPressed == "Clear"){ 
+            displayArray.length = 0;
+            addToScreen(displayArray);
+            return;
+        }
+
         if(buttonPressed == "+" || buttonPressed == "-" || buttonPressed == "*" || buttonPressed == "&#247"){ 
             let prevOperand = displayArray[displayArray.length - 1];
             if(prevOperand == "+" || prevOperand == "-" || prevOperand == "*" || prevOperand == "&#247"){ 
@@ -64,22 +96,20 @@ function calculate(){
         else{ 
             displayArray.push(buttonPressed);
         }
-        return displayArray;
+        addToScreen(displayArray);
+
     }
 
     const allButtons = Array.from(document.querySelectorAll("button")); 
+
     for(let i = 0; i < allButtons.length - 1; i++){ 
-    allButtons[i].addEventListener("click", (e) => { 
-        const buttonPressed = e.target.textContent; 
-        addToCalculations(buttonPressed, displayArray);
-        addToScreen(buttonPressed, displayArray);
-    }); 
-}
-}
+        allButtons[i].addEventListener("click", (e) => { 
+            const buttonPressed = e.target.textContent; 
+            addToCalculations(buttonPressed, displayArray);
+        }); 
+    }
 
-
+    }
 const intOne = 5; 
 const intTwo = 2; 
-
-console.log(operate(intOne, "subtract", intTwo));
 calculate();
